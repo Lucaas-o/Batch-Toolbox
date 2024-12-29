4795fc7114405b28bef34d0ac4dbe7a39c7267dd
LucasElBobo <95099680+LucasElBobo@users.noreply.github.com>
2024-12-29 13:33:41 +0100

    Updated password_generator.bat
    txt file creation, output, copy to clipboard

--- tools/password_generator.bat
+++ tools/password_generator.bat
@@ -8,15 +8,29 @@ cls
 
 setlocal enabledelayedexpansion
 :loop
+:: Set password length (you can adjust this number)
 set /a length=12
 set "chars=abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()"
 set "password="
+
+:: Generate the password
 for /l %%i in (1,1,%length%) do (
     set /a rand=!random! %% 72
     set "password=!password!!chars:~!rand!,1!"
 )
+
+:: Output the generated password
 echo Generated Password: !password!
 
+:: Save the password to a text file
+echo !password! > generated_password.txt
+echo Password saved to generated_password.txt
+
+:: Copy the password to clipboard
+echo !password! | clip
+echo Password copied to clipboard.
+
+:: Ask if the user wants to generate another password
 set /p more="Do you want to generate another password? (y/n): "
 if /i "%more%"=="y" goto loop
 
