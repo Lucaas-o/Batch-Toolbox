5f46adcd77dedfdfb06c87b585998e56190bb0e9
LucasElBobo <95099680+LucasElBobo@users.noreply.github.com>
2024-12-29 13:40:41 +0100

    Update folder_hider.bat
    Test

--- tools/folder_hider.bat
+++ tools/folder_hider.bat
@@ -1,15 +1,25 @@
 @echo off
-:loop
-set /p folder="Enter folder path to hide: "
-attrib +h "%folder%"
-echo Folder hidden.
+echo Hiding file...
 
-set /p more="Do you want to hide another folder? (y/n): "
+:loop
+set /p action="Do you want to hide or reveal a folder? (h/r): "
+
+if /i "%action%"=="h" (
+    set /p folder="Enter folder path to hide: "
+    attrib +h "%folder%"
+    echo Folder hidden.
+) else if /i "%action%"=="r" (
+    set /p folder="Enter folder path to reveal: "
+    attrib -h "%folder%"
+    echo Folder revealed.
+) else (
+    echo Invalid choice. Please enter "h" to hide or "r" to reveal.
+    goto loop
+)
+
+set /p more="Do you want to hide or reveal another folder? (y/n): "
 if /i "%more%"=="y" goto loop
 
 pause
-@echo off 
-cd..
-start toolbox.bat
-timeout /t 1 /nobreak >nul
+
 exit
