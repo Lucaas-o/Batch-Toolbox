6a0d64f404313c998c0099fa4d0c84bb14738327
LucasElBobo <95099680+LucasElBobo@users.noreply.github.com>
2024-12-29 13:27:01 +0100

    Updated password generator
    Fixed bugs

--- tools/password_generator.bat
+++ tools/password_generator.bat
@@ -12,10 +12,10 @@ set /a length=12
 set "chars=abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()"
 set "password="
 for /l %%i in (1,1,%length%) do (
-    set /a rand=!random!%%72
+    set /a rand=!random! %% 72
     set "password=!password!!chars:~!rand!,1!"
 )
-echo Generated Password: %password%
+echo Generated Password: !password!
 
 set /p more="Do you want to generate another password? (y/n): "
 if /i "%more%"=="y" goto loop
