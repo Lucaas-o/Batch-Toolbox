69e212191c0c584e597c089ef0b8649664271eb3
LucasElBobo <95099680+LucasElBobo@users.noreply.github.com>
2024-12-29 13:28:30 +0100

    Final version of password_generator.bat
    Works

--- tools/password_generator.bat
+++ tools/password_generator.bat
@@ -1,27 +1,42 @@
 @Echo Off
-Setlocal EnableDelayedExpansion
-Set _RNDLength=16  :: Length of the password
-Set _Alphanumeric=ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789
-Set _Str=%_Alphanumeric%987654321
-:_LenLoop
-IF NOT "%_Str:~18%"=="" SET _Str=%_Str:~9%& SET /A _Len+=9& GOTO :_LenLoop
-SET _tmp=%_Str:~9,1%
-SET /A _Len=_Len+_tmp
-Set _count=0
-SET _RndAlphaNum=
-:_loop
-Set /a _count+=1
-SET _RND=%Random%
-Set /A _RND=_RND%%%_Len%
-SET _RndAlphaNum=!_RndAlphaNum!!_Alphanumeric:~%_RND%,1!
-If !_count! lss %_RNDLength% goto _loop
+SetLocal EnableExtensions EnableDelayedExpansion
 
-:: Output the password to the command line
-Echo Generated Password: !_RndAlphaNum!
+:: Initialize the character set
+Set "i=-1" 
+For %%_ In (A B C D F G H I J K L M N P Q R S T U V W X Y Z 0 1 2 3 4 5 6 7 8 9) Do (
+    Set /A i += 1
+    Set "$.!i!=%%_"
+)
 
-:: Save the password to a .txt file
-echo !_RndAlphaNum! > generated_password.txt
-Echo Password saved to generated_password.txt
+:: Ask the user to select password length between 8 and 32
+Set /P "length=Enter the password length (between 8 and 32): "
+
+:: Validate the entered length
+If %length% GEQ 8 If %length% LEQ 32 (
+    Echo You have selected a password length of %length% characters.
+) Else (
+    Echo Invalid input. Please enter a number between 8 and 32.
+    GoTo :LengthInput
+)
+
+:: Generate random password of selected length
+Set "strAN="
+For /L %%_ In (1 1 %length%) Do (
+    Set /A randIndex=!random! %% i
+    Set "char=!$.!randIndex!!"
+    Set "strAN=!strAN!!char!"
+)
+
+:: Output the generated password
+Echo Your generated password: %strAN%
+
+:: Ask if the user wants to save the password to a file
+set /p saveFile="Save password to file? (Y/N): "
+if /I "%saveFile%"=="Y" (
+    echo %strAN% > generated_password.txt
+    echo Password saved to generated_password.txt
+)
 
 pause
+start "" toolbox.bat
 exit
